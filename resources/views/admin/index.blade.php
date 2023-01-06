bd5dfdc69d54bd8452604c0ddb47c90ed011791f
matcha <nengindah41@gmail.com>
2023-01-07 06:27:08 +0700

    [HK] set-up view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+{{-- masukan layouts.main --}}
+@extends('layouts.main')
+
+{{-- isi section content --}}
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <h1>Admin Dashboard</h1>
+        </div>
+    </div>
+@endsection
+
