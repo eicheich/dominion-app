4feededcbe428eb6d783a46ace31176846a28bb6
matcha <nengindah41@gmail.com>
2023-02-20 09:34:30 +0700

    [HK]

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,8 +13,8 @@
 </head>
 
 <body>
-    <section class="h-100 gradient-form" style="background-color: rgb(255, 255, 255);">
-        <div class="container py-5 h-100">
+    <section class=" gradient-form" style="background-color: rgb(255, 255, 255);">
+        <div class="container ">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-xl-10">
                     <div class="card rounded-3 text-black">
