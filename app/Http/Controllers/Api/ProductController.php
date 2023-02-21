d635cc5f3b14aa275c876ad1b376e911aeac253c
matcha <nengindah41@gmail.com>
2023-02-22 01:59:33 +0700

    [HK] get product

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return response()->json(
+            [
+                'message' => 'succes',
+                'data' => $products,
+            ],
+            200
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Order\CheckoutController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/callback', [CheckoutController::class, 'callback']);
 
-Route::get('/products', [ProductController::class, 'products'])->name('products');
+// Route::get('/products', [ProductController::class, 'index']);
+
+Route::group(['prefix' => 'v1'], function() {
+    Route::apiResource('/product', ProductController::class);
+} );
